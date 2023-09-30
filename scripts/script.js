d51256e48991c7b6daf3033379484f0ae0593608
samad-ahmed <samadofficial2023@gmail.com>
2023-09-30 15:49:50 +0600

    new project

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+let navList = document.getElementById("navList");
+let openMenu = document.getElementById("openMenu");
+let closeMenu = document.getElementById("closeMenu");
+
+if (openMenu) {
+  openMenu.addEventListener("click", () => {
+    navList.classList.add("active")
+  })
+}
+
+if (closeMenu) {
+  closeMenu.addEventListener("click", () => {
+    navList.classList.remove("active")
+  })
+}
