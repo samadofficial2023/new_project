139814424a4ef6da2a957308734b3e7b9105fa55
samad-ahmed <samadofficial2023@gmail.com>
2023-10-01 20:31:24 +0600

    some added items

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,32 @@
+// let navList = document.getElementById("navList");
+// let openMenu = document.getElementById("openMenu");
+// let closeMenu = document.getElementById("closeMenu");
+
+// if (openMenu) {
+//   openMenu.addEventListener("click", () => {
+//     navList.classList.add("active")
+//   })
+// }
+
+// if (closeMenu) {
+//   closeMenu.addEventListener("click", () => {
+//     navList.classList.remove("active")
+//   })
+// }
+
 let navList = document.getElementById("navList");
 let openMenu = document.getElementById("openMenu");
 let closeMenu = document.getElementById("closeMenu");
 
+
 if (openMenu) {
   openMenu.addEventListener("click", () => {
-    navList.classList.add("active")
+    navList.classList.add("active");
   })
 }
 
 if (closeMenu) {
   closeMenu.addEventListener("click", () => {
-    navList.classList.remove("active")
+    navList.classList.remove("active");
   })
 }
