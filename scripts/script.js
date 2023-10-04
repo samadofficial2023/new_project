579cfd64e86e1f5d2a48e4392058a14b2cc5bdc1
samad-ahmed <samadofficial2023@gmail.com>
2023-10-04 11:50:09 +0600

    new edit

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,3 @@
-// let navList = document.getElementById("navList");
-// let openMenu = document.getElementById("openMenu");
-// let closeMenu = document.getElementById("closeMenu");
-
-// if (openMenu) {
-//   openMenu.addEventListener("click", () => {
-//     navList.classList.add("active")
-//   })
-// }
-
-// if (closeMenu) {
-//   closeMenu.addEventListener("click", () => {
-//     navList.classList.remove("active")
-//   })
-// }
-
 let navList = document.getElementById("navList");
 let openMenu = document.getElementById("openMenu");
 let closeMenu = document.getElementById("closeMenu");
@@ -30,3 +14,5 @@ if (closeMenu) {
     navList.classList.remove("active");
   })
 }
+
+
